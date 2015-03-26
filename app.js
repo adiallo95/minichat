25bc8dd3daf172f84168706959b7a0cf377af4d4
Mathieu Nebra <mathieu@nebra.fr>
2015-03-26 10:53:46 +0100

    Updates socket.io version

--- app.js
+++ app.js
@@ -17,16 +17,14 @@ io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
-        socket.set('pseudo', pseudo);
+        socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
-        socket.get('pseudo', function (error, pseudo) {
-            message = ent.encode(message);
-            socket.broadcast.emit('message', {pseudo: pseudo, message: message});
-        });
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 });
 
